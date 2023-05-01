1eb613609b2efdaa9229e1c45d3a9114cb6f2abb
LeeSeungYeon <masakisqq@naver.com>
2023-05-01 16:07:53 +0900

    회원가입 - 실명확인 페이지

--- src/main/webapp/resources/JS/semiprojectv4.js
+++ src/main/webapp/resources/JS/semiprojectv4.js
@@ -4,6 +4,8 @@ logo.addEventListener('click', () => {
     location.href = '/';
 })
 
+// ------------------------------------------ agree
+
 // 이용약관 체크박스
 let agreeone = document.querySelector('#agreeone');
 
@@ -23,3 +25,26 @@ let noagree = document.querySelector('#noagree');
 noagree?.addEventListener('click', () => {
     location.href = '/';
 })
+
+// ------------------------------------------ checkme
+const name2 = document.querySelector('#name2');
+const jumin1 = document.querySelector('#jumin1');
+const jumin2 = document.querySelector('#jumin2');
+const chkjumin = document.querySelector('#chkjumin');
+const chkbtn2 = document.querySelector('#check2btn');
+const chkfrm2 = document.querySelector('#checkfrm2');
+chkbtn2?.addEventListener('click', () => {
+    if (name2.value == '') alert('이름을 입력하세요!!');
+    else if (jumin1.value == '') alert('주민번호를 입력하세요!');
+    else if (jumin2.value == '') alert('나머지 주민번호를 입력하세요!!');
+    else if (!chkjumin.checked) alert('주민번호 처리에 동의하세요!!');
+    else {
+        chkfrm2.method = 'post';
+        chkfrm2.action = '/join/joinme';
+        chkfrm2.submit();
+    }
+})
+
+// ------------------------------------------ joinme
+
+// ------------------------------------------ joinok
